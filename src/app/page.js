e07fb122508320f3b73c352684c090b2998cc592
DylanCerrudo <dc36040@email.vccs.edu>
2025-06-09 16:30:19 -0400

    Fix apostrophe with HTML entity

--- src/app/page.js
+++ src/app/page.js
@@ -23,7 +23,7 @@ export default function Home() {
           boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)",
         }}
       >
-                <h1 style={{ margin: 1 }}>Welcome to Dylan&apos;s Pantry Tracker!</h1>
+                <h1 style={{ margin: 1 }}>Welcome to Dylans Pantry Tracker!</h1>
 
       </div>
 
